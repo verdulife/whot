72ef2c9860e7bd6217d0047c88b167a7343b897f
akuario <akuarioteam@gmail.com>
2019-01-10 08:48:32 +0100

    muevete

--- app/game/game.js
+++ app/game/game.js
@@ -1,13 +1,28 @@
 angular
   .module("app")
-  .controller("gameController", function(
-    $scope,
-    $state,
-    $stateParams
-  ) {
+  .controller("gameController", function($scope, $state, $stateParams) {
     info("Game loaded");
 
     $scope.back = () => $state.go("menu");
     $scope.getWhot = $stateParams.whot;
     $scope.game = {};
+
+    window.ondevicemotion = function(e) {
+      let zAxis = (document.getElementById("accelerationZ").innerHTML =
+        e.accelerationIncludingGravity.z);
+
+      if (zAxis <= -5) {
+        document.getElementById("wait").style.display = "none";
+        document.getElementById("front").style.display = "none";
+        document.getElementById("back").style.display = "inherit";
+      } else if (zAxis >= 5) {
+        document.getElementById("wait").style.display = "none";
+        document.getElementById("front").style.display = "inherit";
+        document.getElementById("back").style.display = "none";
+      } else {
+        document.getElementById("wait").style.display = "inherit";
+        document.getElementById("front").style.display = "none";
+        document.getElementById("back").style.display = "none";
+      }
+    };
   });
