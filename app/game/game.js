4feb4dfb953df99f49c365ad3acdda8be7708960
verdulife <verdu@live.com>
2019-01-11 18:19:02 +0100

    game fn 2

--- app/game/game.js
+++ app/game/game.js
@@ -12,72 +12,66 @@ angular
 
     $scope.back = () => $state.go("menu");
     $scope.getWhot = $stateParams.whot;
-    $scope.game = {};
-    let timer = $stateParams.time;
+    let time = $stateParams.time;
+    let timer = document.getElementById("timer");
+    let zAxis = document.getElementById("accelerationZ");
+
+    timer.innerHTML = "Preparate";
 
     let selectedWhot = server.db.collection("whots").doc($scope.getWhot);
-
     selectedWhot.get().then(doc => {
       $scope.$apply(() => {
         $scope.whot = doc.data();
         succ("Data obtained");
+
+        $timeout(() => $scope.message = "Preparados...");
+        $timeout(() => $scope.message = "Listos...", 1000);
+        $timeout(() => $scope.message = "¡Ya!", 2000);
+
+        let countDown = $timeout(() => {
+          let random = Math.floor(Math.random() * $scope.whot.whots.length);
+          $scope.message = $scope.whot.whots[random];
+          timer.innerHTML = time;
+
+          $interval(() => {
+            if (time > 0) {
+              time -= 1;
+              timer.innerHTML = time;
+            } else {
+              $timeout(
+                () => (timer.innerHTML = "¡Tiempo!"),
+                100
+              );
+            }
+          }, 1000);
+
+        }, 3000);
+
+        window.ondevicemotion = function(e) {
+          zAxis.innerHTML = e.accelerationIncludingGravity.z;
+          let random = Math.floor(Math.random() * $scope.whot.whots.length);
+          let rigthWhots = [];
+          let wrongWhots = [];
+
+          if (zAxis <= -5) {
+            let wrong = true;
+            if ((wrong = true)) {
+              wait.style.display = "none";
+              hit.style.display = "none";
+              pass.style.display = "inherit";
+              $scope.message = $scope.whot.whots[random];
+            }
+          } else if (zAxis >= 4) {
+            wait.style.display = "none";
+            hit.style.display = "inherit";
+            pass.style.display = "none";
+            $scope.message = $scope.whot.whots[random];
+          } else {
+            wait.style.display = "inherit";
+            hit.style.display = "none";
+            pass.style.display = "none";
+          }
+        };
       });
     });
-
-    document.getElementById("timer").innerHTML = "¡3!";
-    $timeout(() => (document.getElementById("timer").innerHTML = "¡2!"), 1000);
-    $timeout(() => (document.getElementById("timer").innerHTML = "¡1!"), 2000);
-
-    $timeout(() => {
-      document.getElementById("timer").innerHTML = timer;
-      let random = Math.floor(Math.random() * $scope.whot.whots.length);
-      $scope.random = $scope.whot.whots[random];
-
-      $interval(() => {
-        if (timer > 0) {
-          timer -= 1;
-          document.getElementById("timer").innerHTML = timer;
-        } else {
-          $timeout(
-            () => (document.getElementById("timer").innerHTML = "¡Tiempo!"),
-            100
-          );
-        }
-      }, 1000);
-    }, 3000);
-
-    let wait = document.getElementById("wait");
-    let hit = document.getElementById("hit");
-    let pass = document.getElementById("pass");
-
-    wait.style.display = "inherit";
-    hit.style.display = "none";
-    pass.style.display = "none";
-
-    window.ondevicemotion = function(e) {
-      let zAxis = (document.getElementById("accelerationZ").innerHTML =
-        e.accelerationIncludingGravity.z);
-      let random = Math.floor(Math.random() * $scope.whot.whots.length);
-      let rigthWhots = [];
-      let wrongWhots = [];
-
-      if (zAxis <= -5) {
-        let wrong = true;
-        if(wrong = true) {
-          wait.style.display = "none";
-        hit.style.display = "none";
-        pass.style.display = "inherit";
-        $scope.random = $scope.whot.whots[random];
-        }
-      } else if (zAxis >= 4) {
-        wait.style.display = "none";
-        hit.style.display = "inherit";
-        pass.style.display = "none";
-        $scope.random = $scope.whot.whots[random];
-      } else {
-        wait.style.display = "inherit";
-        hit.style.display = "none";
-        pass.style.display = "none";
-      }
-    };
   });
